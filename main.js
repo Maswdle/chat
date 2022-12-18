26a72636c34be147207d2c69b2445542229d9d92
Maswdle <wondr009@163.com>
2022-12-18 13:05:49 +0800

    寄

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// const axios = require('axios').default;
-// import qs from 'qs'
+
 document.getElementById("enter_a").addEventListener("click", enter_a_click);
 document.getElementById("enter_b").addEventListener("click", enter_b_click);
 
@@ -22,12 +21,6 @@ function id_check() {
     return false;
 }
 
-
-axios.defaults.withCredentials = true // 若跨域请求需要带 cookie 身份识别
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-
-
-
 function enter_a_click() {
     userID = document.getElementById("id_input").value;
     if (!id_check()) {
@@ -35,32 +28,32 @@ function enter_a_click() {
         userID = "";
         return;
     }
-    axios(
-        //todo 伪造Referer
-        {
-            // headers: {
-            //     'Referer': 'http://v2.ozvzzs.cn/randomdeskrynewafwekb.html?v=afwekb'
-            // },
-            method: 'get',
-            url: 'http://requestbin.cn:80/stomm0st',
-            // url: 'http://v1.nmcp.cn:668/asmx/method.asmx/randomVIPGetHistoryUserList_Random/',
-            // data:
-            // {
-            //     myUserID: userID,
-            //     vipcode: "",
-            //     severPort: "1001"
-            // }
-
+    http.post({
+        url: "http://v1.nmcp.cn:668/asmx/method.asmx/randomVIPGetHistoryUserList_Random",
+        data: {
+            myUserID: userID,
+            vipcode: "",
+            severPort: 1001
+        },
+        headers: {
+            'Referer': 'http://v2.ozvzzs.cn/randomdeskrynewafwekb.html?v=afwekb'
+        },
+        timeout: 1000
+    }, function (err, result) {
+        var obj = document.getElementById("select1");
+        obj.options.length = 0; //清空列表
+        for (var p in result) {
+            obj.options.add(new Option(res_json[p].nickname));
         }
+    })
 
-    )
-        // .then((result) => {
-        //     var obj = document.getElementById("select1");
-        //     obj.options.length = 0; //清空列表
-        //     for (var p in result) {
-        //         obj.options.add(new Option(res_json[p].nickname));
-        //     }
-        // })
+    // .then((result) => {
+    //     var obj = document.getElementById("select1");
+    //     obj.options.length = 0; //清空列表
+    //     for (var p in result) {
+    //         obj.options.add(new Option(res_json[p].nickname));
+    //     }
+    // })
 }
 
 function enter_b_click() {
